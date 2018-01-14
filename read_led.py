f75e6d9b2ec0cdf5c6e0029954ca0bc9e5a5d824
Matthew Upson <matthew.a.upson@gmail.com>
2018-01-14 19:47:02 +0100

    Simplify read_led.py
    Remove extraneous code. Simplify this script to do just one thing:
    record and log led flashes. Calculations of initial values, and the kWh
    values will be made elsewhere.

--- read_led.py
+++ read_led.py
@@ -1,112 +1,107 @@
+'''
+Read a blinking LED and log
+'''
+
 #!/usr/bin/python
 
+from time import strftime, sleep, time
+#from socket import gethostname
 import RPi.GPIO as GPIO
-#from time import sleep
-from time import strftime,sleep,time
-from socket import gethostname
-import string, os, sys, sqlite3, psycopg2, server_cred
 
 ######################### Setup GPIO PINS #########################
 
 # Use Broadcom chip reference for GPIO
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 # Name pins
 
-pin4 = 17       # Photoresistor
+LDR_PIN = 17    # LDR (light dependent resistor)
+LED_PIN = 0     # LED (light emitting diode)
 
-GPIO.setup(pin4, GPIO.IN)
+GPIO.setup(LDR_PIN, GPIO.IN)
 
 # Define functions
 
-def getLight():
-	input_value = GPIO.input(pin4)
-	# Sleep value can be experimented with, but 0.03 seems to be reasonable
-	sleep(0.03)
-	if (input_value == 0):
-		# 0.001 is the value in kWh that one flash represents
-		return(0.001)
-	else:
-		return(0)
+def get_light(LDR_PIN, duration=0.03):
+    '''
+    Read from the GPIO pin then sleep
+
+    :param LDR_PIN: <int> Address of GPIO pin connected to light dependent resistor
+    :param duration: <float> Duration in seconds that the rpi will sleep beyween measurements
+    :return: <boolean>
+    '''
+
+    input_value = GPIO.input(LDR_PIN)
+
+    # Duration value can be experimented with
+    # but 0.03 seems to be reasonable
+
+    sleep(duration)
+    if input_value == 0:
+        output = 1
+    else:
+        output = 0
+    return output
 
 # Run data recording LED init sequence
 # This is not currently used in this version
 
-def ledFlash(i,j):
-	ledCount = 0
-	while ledCount < i:
-		GPIO.output(pin2, GPIO.HIGH)
-		sleep(j)
-		GPIO.output(pin2, GPIO.LOW)
-		sleep(j)
-		ledCount +=1
+def ledFlash(LED_PIN, n_flashes=2, interval=0.3):
+    '''
+    Flash an indicator LED
 
-def write_log_csv(ts,day,night,val):
-        log = open("/home/pi/elec/Log.csv", "a")
-	log.write("\n" + str(ts) + "," + str(day) + "," + str(night) + "," + str(val))
-	log.close()
+    :param LED_PIN: <int> Address of pin connected to LED
+    :param n_flashes: <int> number of times to flash LED
+    :param interval: <int> Interval between flashes (in seconds)
+    '''
 
-def write_log_psql(ts,day,night,val):
+    count = 0
+    while count < n_flashes:
+        GPIO.output(LED_PIN, GPIO.HIGH)
+        sleep(interval)
+        GPIO.output(LED_PIN, GPIO.LOW)
+        sleep(interval)
+        led_count += 1
 
-        conn_string = str("dbname = '"+ server_cred.db_name + "' user = '" + server_cred.username + "' host = '" + server_cred.host_ip + "' password = '" + server_cred.password + "'")
-        conn = psycopg2.connect(conn_string)
-        curs = conn.cursor()
-        query_string = str("INSERT INTO elec values('" + str(ts) + "','" + str(day) + "','" + str(night) + "','" + str(val) + "');")
-        curs.execute(query_string)
-        conn.commit()
-        curs.close()
-        conn.close()
+def write_log_csv(ts, val):
+    '''
+    Write log data to csv
+    '''
 
-def main():
+    log = open("/home/pi/elec/Log.csv", "a")
+    log.write("\n" + str(ts) + "," + str(val))
+    log.close()
 
-	# First read initial values (i.e. your meters current electricity use)
+def main(interval=60):
+    '''
+    Run the counter
 
-        initial = open("/home/pi/elec/initial.vals","r")
-        day = float(initial.readline())
-        night = float(initial.readline())
-        initial.close()
+    :param interval: <int> Recording interval (defaults to 60: one reading every minute)
+    '''
 
-	# Set the timeout to be the current time plus 60 seconds
+    # Set the timeout to be the current time plus 60 seconds
 
-	while True:
-		timeout = time() + 60
-	# Initiliase accumulator at zero
-	# Then run getLight and add to accumulator
-		counter = 0
-		while timeout > time():
-			counter += getLight()
+    while True:
+        timeout = time() + interval
 
-                timestamp = strftime("%Y-%m-%d %H:%M:%S")
+    # Initiliase accumulator at zero
+    # Then run get_light and add to accumulator
+        counter = 0
+        while timeout > time():
+            counter += get_light(LDR_PIN)
 
-	# This is only useul if you have an economy seven meter.
-	# These values can be changed to be more exact relative
-	# to your switchover time
+        timestamp = strftime("%Y-%m-%d %H:%M:%S")
 
-		if int(strftime("%H")) in range(7,22):
-			day += counter
-		else:
-			night += counter
+    # Try to log to csv
 
-	# Try to log to psql database
-
-		try:
-			write_log_psql(timestamp,day,night,counter)
-		except:
-			pass
-
-	# Also try to log to csv
-
-		try:
-			write_log_csv(timestamp,day,night,counter)
-		except:
-			pass
-
-        	initial = open("/home/pi/elec/initial.vals","w")
-        	initial.write(str(day) + "\n" + str(night))
-        	initial.close()
+        try:
+            write_log_csv(timestamp, counter)
+        except Exception:
+            pass
 
 if __name__ == '__main__':
-	main()
+    main()
 
 GPIO.cleanup()
